71c59e51702ae18fbc039cd7e5be58d7c62a9023
Taeyang Kim <k2289@byu.edu>
2022-05-11 13:19:23 -0600

    move them to folders

---
+++ practice/script.js
@@ -0,0 +1,47 @@
+Vue.compent('price', {
+  data: function () {
+    return {
+      prefix: '$',
+      value: 22.34,
+      precision: 2
+    }
+  },
+  props: {
+    value: Number,
+    prefix: {
+      type: String,
+      default: '$'
+    },
+    precision: {
+      type: Number,
+      default: 2
+    },
+    conversion: {
+      type: Number,
+      default: 2
+    }
+  },
+  //You can pass along objects for props
+  template: '<span> {{ this.prefix + Number.paseFloat(this.value * this.conversion).toFixed(this.precision) }} </span>'
+});
+
+Vue.compent('product-list', {
+  data: function () {
+    return {
+      prefix: '$',
+      value: 22.34,
+      precision: 2
+    }
+  },
+  props: ['products', 'maximum'],
+  template: '<span> {{ this.prefix + Number.paseFloat(this.value * this.conversion).toFixed(this.precision) }} </span>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    inputwidth: 60,
+  },
+  props: [],
+});
+
